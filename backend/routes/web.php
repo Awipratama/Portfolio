ad27e7d3a89e6a14053dfa6f229c1a20416dee93
awipratama <awipratama05122006@gmail.com>
2025-08-05 05:13:00 +0800

    backend update

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TableController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('dashboard');
 });
+Route::get('/table', [TableController::class, 'table'])->name('table');
